0d78cba7e5b0d4472f87ad58882a58fd2be088dc
Michael Wallner <Michael.Wallner@Sundsvall.se>
2023-01-31 15:02:37 +0100

    UF-4312: Added config of spring-filter and created tests to verify

--- src/test/java/se/sundsvall/supportmanagement/integration/db/ErrandsRepositoryTest.java
+++ src/test/java/se/sundsvall/supportmanagement/integration/db/ErrandsRepositoryTest.java
@@ -11,19 +11,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-
 import se.sundsvall.supportmanagement.api.model.errand.CustomerType;
 import se.sundsvall.supportmanagement.integration.db.model.DbExternalTag;
 import se.sundsvall.supportmanagement.integration.db.model.EmbeddableCustomer;
 import se.sundsvall.supportmanagement.integration.db.model.ErrandEntity;
 
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.api.Assertions.within;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Tag repository tests.
@@ -125,6 +126,47 @@ class ErrandsRepositoryTest {
 		assertThat(errandEntities)
 			.extracting(ErrandEntity::getId, ErrandEntity::getAssignedGroupId, ErrandEntity::getAssignedUserId).containsExactlyInAnyOrder(
 				tuple("ERRAND_ID-1", "ASSIGNED_GROUP_ID-1", "ASSIGNED_USER_ID-1"));
+	}
+
+	@Test
+	void findByDateFilter() {
+		//Setup date filter
+		final var dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+		final var from = OffsetDateTime.now().minusMinutes(1).format(dateFormat);
+		final var to = OffsetDateTime.now().plusMinutes(1).format(dateFormat);
+		final var filter = String.format("(created > '" + from + "' AND created < '" + to + "')");
+
+		Specification<ErrandEntity> specification = new FilterSpecification<>(filter);
+		Pageable pageable = PageRequest.of(0, 20);
+
+		//Setup ErrandEntity to find
+		final var entityToUpdate = errandsRepository.findById("ERRAND_ID-3");
+		entityToUpdate.get().setCreated(OffsetDateTime.now());
+		errandsRepository.save(entityToUpdate.get());
+
+		final var errandEntities = errandsRepository.findAll(specification, pageable);
+
+		assertThat(errandEntities).isNotNull();
+		assertThat(errandEntities.getTotalElements()).isEqualTo(1);
+
+		assertThat(errandEntities)
+			.extracting(ErrandEntity::getId, ErrandEntity::getAssignedGroupId, ErrandEntity::getAssignedUserId).containsExactlyInAnyOrder(
+				tuple("ERRAND_ID-3", "ASSIGNED_GROUP_ID-3", "ASSIGNED_USER_ID-3"));
+	}
+
+	@Test
+	void shouldThrowExceptionWhenDateTimeIsOfWrongFormat() {
+		//Setup date filter;
+		final var invalidDateFormat = "31-01-2023T16:00:20.954+01:00";
+		final var filter = String.format("(created > '" + invalidDateFormat + "')");
+
+		Specification<ErrandEntity> specification = new FilterSpecification<>(filter);
+		Pageable pageable = PageRequest.of(0, 20);
+
+		final var exception = assertThrows(Throwable.class, () -> errandsRepository.findAll(specification, pageable));
+
+		assertThat(exception).isNotNull().isInstanceOf(ClassCastException.class);
+		assertThat(exception.getMessage()).isEqualTo("The input '31-01-2023T16:00:20.954+01:00' could not be parsed to OffsetDateTime");
 	}
 
 	@Test
