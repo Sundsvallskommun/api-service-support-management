c81cd157d84a5d11546260262a982678e269906a
Joakim Schürer <joakim.schurer@sundsvall.se>
2023-03-28 09:35:26 +0200

    UF-4541: Added IT tests for externalid type and status metadata

---
+++ src/integration-test/java/se/sundsvall/supportmanagement/apptest/MetadataExternalIdTypeIT.java
@@ -0,0 +1,110 @@
+package se.sundsvall.supportmanagement.apptest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+import se.sundsvall.dept44.test.AbstractAppTest;
+import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
+import se.sundsvall.supportmanagement.Application;
+import se.sundsvall.supportmanagement.integration.db.ExternalIdTypeRepository;
+
+/**
+ * Status Metadata IT tests.
+ */
+@WireMockAppTestSuite(files = "classpath:/MetadataExternalIdTypeIT/", classes = Application.class)
+@Sql({
+	"/db/scripts/truncate.sql",
+	"/db/scripts/testdata-it.sql"
+})
+class MetadataExternalIdTypeIT extends AbstractAppTest {
+	private static final String REQUEST_FILE = "request.json";
+	private static final String RESPONSE_FILE = "response.json";
+	private static final String NAMESPACE = "NAMESPACE.1";
+	private static final String MUNICIPALITY_2281 = "2281";
+	private static final String MUNICIPALITY_2309 = "2309";
+	private static final String PATH = "/" + NAMESPACE + "/" + MUNICIPALITY_2281 + "/metadata/externalIdTypes";
+
+	@Autowired
+	private ExternalIdTypeRepository externalIdTypeRepository;
+
+	@Test
+	void test01_createExternalIdType() throws Exception {
+		setupCall()
+			.withServicePath(PATH)
+			.withHttpMethod(POST)
+			.withRequest(REQUEST_FILE)
+			.withExpectedResponseStatus(CREATED)
+			.withExpectedResponseHeader(LOCATION, List.of("^http://(.*)/metadata/externalIdTypes/A_BRAND_NEW_EXTERNAL_ID_TYPE$"))
+			.withExpectedResponseBodyIsNull()
+			.sendRequestAndVerifyResponse();
+
+		assertThat(externalIdTypeRepository.existsByNamespaceAndMunicipalityIdAndName(NAMESPACE, MUNICIPALITY_2281, "A_BRAND_NEW_EXTERNAL_ID_TYPE")).isTrue();
+	}
+
+	@Test
+	void test02_getExternalIdType() throws Exception {
+		setupCall()
+			.withServicePath(PATH + "/EXTERNAL-ID-TYPE-3")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponseHeader(CONTENT_TYPE, List.of(APPLICATION_JSON_VALUE))
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test03_getExternalIdTypes() throws Exception {
+		setupCall()
+			.withServicePath(PATH)
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponseHeader(CONTENT_TYPE, List.of(APPLICATION_JSON_VALUE))
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test04_getExternalIdTypesWhenEmpty() throws Exception {
+		final var path = "/" + NAMESPACE + "/" + MUNICIPALITY_2309 + "/metadata/externalIdTypes";
+
+		setupCall()
+			.withServicePath(path)
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponseHeader(CONTENT_TYPE, List.of(APPLICATION_JSON_VALUE))
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test05_deleteExternalIdType() throws Exception {
+		final var externalIdTypeName = "EXTERNAL-ID-TYPE-3";
+
+		assertThat(externalIdTypeRepository.existsByNamespaceAndMunicipalityIdAndName(NAMESPACE, MUNICIPALITY_2281, externalIdTypeName)).isTrue();
+		assertThat(externalIdTypeRepository.count()).isEqualTo(6);
+		
+		setupCall()
+			.withServicePath(PATH + "/" + externalIdTypeName)
+			.withHttpMethod(DELETE)
+			.withExpectedResponseStatus(NO_CONTENT)
+			.withExpectedResponseBodyIsNull()
+			.sendRequestAndVerifyResponse();
+
+		assertThat(externalIdTypeRepository.existsByNamespaceAndMunicipalityIdAndName(NAMESPACE, MUNICIPALITY_2281, externalIdTypeName)).isFalse();
+		assertThat(externalIdTypeRepository.count()).isEqualTo(5);
+	}
+}
---
+++ src/integration-test/java/se/sundsvall/supportmanagement/apptest/MetadataStatusIT.java
@@ -0,0 +1,111 @@
+package se.sundsvall.supportmanagement.apptest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+import se.sundsvall.dept44.test.AbstractAppTest;
+import se.sundsvall.dept44.test.annotation.wiremock.WireMockAppTestSuite;
+import se.sundsvall.supportmanagement.Application;
+import se.sundsvall.supportmanagement.integration.db.StatusRepository;
+
+/**
+ * Status Metadata IT tests.
+ */
+@WireMockAppTestSuite(files = "classpath:/MetadataStatusIT/", classes = Application.class)
+@Sql({
+	"/db/scripts/truncate.sql",
+	"/db/scripts/testdata-it.sql"
+})
+class MetadataStatusIT extends AbstractAppTest {
+	private static final String REQUEST_FILE = "request.json";
+	private static final String RESPONSE_FILE = "response.json";
+	private static final String NAMESPACE = "NAMESPACE.1";
+	private static final String MUNICIPALITY_2281 = "2281";
+	private static final String MUNICIPALITY_2309 = "2309";
+	private static final String PATH = "/" + NAMESPACE + "/" + MUNICIPALITY_2281 + "/metadata/statuses";
+
+	@Autowired
+	private StatusRepository statusRepository;
+
+	@Test
+	void test01_createStatus() throws Exception {
+		setupCall()
+			.withServicePath(PATH)
+			.withHttpMethod(POST)
+			.withRequest(REQUEST_FILE)
+			.withExpectedResponseStatus(CREATED)
+			.withExpectedResponseHeader(LOCATION, List.of("^http://(.*)/metadata/statuses/A_BRAND_NEW_STATUS$"))
+			.withExpectedResponseBodyIsNull()
+			.sendRequestAndVerifyResponse();
+
+		assertThat(statusRepository.existsByNamespaceAndMunicipalityIdAndName(NAMESPACE, MUNICIPALITY_2281, "A_BRAND_NEW_STATUS")).isTrue();
+	}
+
+	@Test
+	void test02_getStatus() throws Exception {
+		setupCall()
+			.withServicePath(PATH + "/STATUS-2")
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponseHeader(CONTENT_TYPE, List.of(APPLICATION_JSON_VALUE))
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test03_getStatuses() throws Exception {
+
+		setupCall()
+			.withServicePath(PATH)
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponseHeader(CONTENT_TYPE, List.of(APPLICATION_JSON_VALUE))
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test04_getStatusesWhenEmpty() throws Exception {
+		final var path = "/" + NAMESPACE + "/" + MUNICIPALITY_2309 + "/metadata/statuses";
+
+		setupCall()
+			.withServicePath(path)
+			.withHttpMethod(GET)
+			.withExpectedResponseStatus(OK)
+			.withExpectedResponseHeader(CONTENT_TYPE, List.of(APPLICATION_JSON_VALUE))
+			.withExpectedResponse(RESPONSE_FILE)
+			.sendRequestAndVerifyResponse();
+	}
+
+	@Test
+	void test05_deleteStatus() throws Exception {
+		final var statusName = "STATUS-2";
+
+		assertThat(statusRepository.existsByNamespaceAndMunicipalityIdAndName(NAMESPACE, MUNICIPALITY_2281, statusName)).isTrue();
+		assertThat(statusRepository.count()).isEqualTo(6);
+		
+		setupCall()
+			.withServicePath(PATH + "/" + statusName)
+			.withHttpMethod(DELETE)
+			.withExpectedResponseStatus(NO_CONTENT)
+			.withExpectedResponseBodyIsNull()
+			.sendRequestAndVerifyResponse();
+
+		assertThat(statusRepository.existsByNamespaceAndMunicipalityIdAndName(NAMESPACE, MUNICIPALITY_2281, statusName)).isFalse();
+		assertThat(statusRepository.count()).isEqualTo(5);
+	}
+}
