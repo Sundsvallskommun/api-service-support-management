ee880a1617a6d540424fa0a2b8a0b863c70cd9b2
cheezi0747 <linus.sjolinder@b3.se>
2023-12-11 23:12:48 +0100

    emailreader integration

---
+++ src/main/java/se/sundsvall/supportmanagement/integration/emailreader/configuration/EmailreaderConfiguration.java
@@ -0,0 +1,23 @@
+package se.sundsvall.supportmanagement.integration.emailreader.configuration;
+
+import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
+import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
+
+public class EmailreaderConfiguration {
+
+	public static final String CLIENT_ID = "emailreader";
+
+	@Bean
+	FeignBuilderCustomizer feignBuilderCustomizer(final EmailReaderProperties emailReaderProperties, final ClientRegistrationRepository clientRegistrationRepository) {
+		return FeignMultiCustomizer.create()
+			.withErrorDecoder(new ProblemErrorDecoder(CLIENT_ID))
+			.withRequestTimeoutsInSeconds(emailReaderProperties.connectTimeout(), emailReaderProperties.readTimeout())
+			.withRetryableOAuth2InterceptorForClientRegistration(clientRegistrationRepository.findByRegistrationId(CLIENT_ID))
+			.composeCustomizersToOne();
+	}
+
+}
