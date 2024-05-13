4c6fe845623c279b68cd5beef36b7ea5a9021f55
cheezi0747 <linus.sjolinder@b3.se>
2024-05-13 14:21:13 +0200

    UF-8277: Employee integration

---
+++ src/main/java/se/sundsvall/supportmanagement/integration/employee/configuration/EmployeeConfiguration.java
@@ -0,0 +1,30 @@
+package se.sundsvall.supportmanagement.integration.employee.configuration;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+import se.sundsvall.dept44.configuration.feign.FeignConfiguration;
+import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
+import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
+
+@Import(FeignConfiguration.class)
+public class EmployeeConfiguration {
+
+	public static final String CLIENT_ID = "employee";
+
+	@Bean
+	FeignBuilderCustomizer feignBuilderCustomizer(final EmployeeProperties employeeProperties, final ClientRegistrationRepository clientRegistrationRepository) {
+		return FeignMultiCustomizer.create()
+			.withErrorDecoder(new ProblemErrorDecoder(CLIENT_ID, List.of(NOT_FOUND.value())))
+			.withRequestTimeoutsInSeconds(employeeProperties.connectTimeout(), employeeProperties.readTimeout())
+			.withRetryableOAuth2InterceptorForClientRegistration(clientRegistrationRepository.findByRegistrationId(CLIENT_ID))
+			.composeCustomizersToOne();
+	}
+
+}
