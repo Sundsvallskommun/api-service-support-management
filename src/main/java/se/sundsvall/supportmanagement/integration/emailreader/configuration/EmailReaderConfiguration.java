c859404e8fb5b38247781fcd800d91a776e86173
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2024-05-13 16:52:30 +0200

    UF-8304: Add missing import on EmailReaderConfiguration

--- src/main/java/se/sundsvall/supportmanagement/integration/emailreader/configuration/EmailReaderConfiguration.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/emailreader/configuration/EmailReaderConfiguration.java
@@ -2,11 +2,14 @@ package se.sundsvall.supportmanagement.integration.emailreader.configuration;
 
 import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
+import se.sundsvall.dept44.configuration.feign.FeignConfiguration;
 import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
 import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
 
+@Import(FeignConfiguration.class)
 public class EmailReaderConfiguration {
 
 	public static final String CLIENT_ID = "emailreader";
