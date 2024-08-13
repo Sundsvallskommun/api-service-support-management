8c44f6b3066892b822892c0b0ab0b4103c082031
AndreSoderlund <andre.soderlund@b3.se>
2024-08-13 10:36:59 +0200

    acted on pr feedback

--- src/main/java/se/sundsvall/supportmanagement/integration/emailreader/EmailReaderClient.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/emailreader/EmailReaderClient.java
@@ -12,23 +12,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import se.sundsvall.supportmanagement.integration.emailreader.configuration.EmailReaderConfiguration;
 
 import generated.se.sundsvall.emailreader.Email;
-import io.swagger.v3.oas.annotations.Parameter;
 
 @FeignClient(name = CLIENT_ID, url = "${integration.emailreader.url}", configuration = EmailReaderConfiguration.class)
 public interface EmailReaderClient {
 
 	@GetMapping("/{municipalityId}/email/{namespace}")
-	List<Email> getEmails(
-		@Parameter(name = "municipalityId", description = "Municipality id", example = "2281")
-		@PathVariable("municipalityId") final String municipalityId,
-		@Parameter(name = "namespace", description = "A specific namespace", example = "CONTACTCENTER")
+	List<Email> getEmails(@PathVariable("municipalityId") final String municipalityId,
 		@PathVariable("namespace") final String namespace);
 
 	@DeleteMapping("/{municipalityId}/email/{id}")
-	void deleteEmail(
-		@Parameter(name = "municipalityId", description = "Municipality id", example = "2281")
-		@PathVariable("municipalityId") final String municipalityId,
-		@Parameter(name = "id", description = "Email message ID", example = "81471222-5798-11e9-ae24-57fa13b361e1")
+	void deleteEmail(@PathVariable("municipalityId") final String municipalityId,
 		@PathVariable("id") final String id);
 
 }
