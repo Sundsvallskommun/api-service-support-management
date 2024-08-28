0b3a6b7e3e78f5ac88bfb59a42b4f34121afedb9
cheezi0747 <linus.sjolinder@b3.se>
2024-08-28 15:33:15 +0200

    UF-9717: Review changes

--- src/main/java/se/sundsvall/supportmanagement/api/ErrandLabelsResource.java
+++ src/main/java/se/sundsvall/supportmanagement/api/ErrandLabelsResource.java
@@ -41,6 +41,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/{namespace}/{municipalityId}/errands/{id}/labels")
 @Tag(name = "Errands", description = "Errand labels operations")
 @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(oneOf = {Problem.class, ConstraintViolationProblem.class})))
+@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 @ApiResponse(responseCode = "500", description = "Internal Server error", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 public class ErrandLabelsResource {
 
@@ -74,7 +75,6 @@ public class ErrandLabelsResource {
 
 	@PutMapping(consumes = APPLICATION_JSON_VALUE, produces = {ALL_VALUE, APPLICATION_PROBLEM_JSON_VALUE})
 	@ApiResponse(responseCode = "204", description = "No content")
-	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 	public ResponseEntity<Void> updateErrandLabels(
 		@Parameter(name = "namespace", description = "Namespace", example = "my.namespace") @Pattern(regexp = NAMESPACE_REGEXP, message = NAMESPACE_VALIDATION_MESSAGE) @PathVariable final String namespace,
 		@Parameter(name = "municipalityId", description = "Municipality id", example = "2281") @ValidMunicipalityId @PathVariable final String municipalityId,
@@ -86,7 +86,6 @@ public class ErrandLabelsResource {
 
 	@DeleteMapping
 	@ApiResponse(responseCode = "204", description = "No content")
-	@ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = Problem.class)))
 	public ResponseEntity<Void> removeErrandLabel(
 		@Parameter(name = "namespace", description = "Namespace", example = "my.namespace") @Pattern(regexp = NAMESPACE_REGEXP, message = NAMESPACE_VALIDATION_MESSAGE) @PathVariable final String namespace,
 		@Parameter(name = "municipalityId", description = "Municipality id", example = "2281") @ValidMunicipalityId @PathVariable final String municipalityId,
