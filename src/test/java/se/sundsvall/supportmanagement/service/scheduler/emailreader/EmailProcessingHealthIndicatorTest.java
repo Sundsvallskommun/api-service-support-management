609c00da2b4db53897075f4ac672ac2d4e951797
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2024-08-28 10:39:45 +0200

    UF-9711: Change error handling for scheduled email polling

---
+++ src/test/java/se/sundsvall/supportmanagement/service/scheduler/emailreader/EmailProcessingHealthIndicatorTest.java
@@ -0,0 +1,46 @@
+package se.sundsvall.supportmanagement.service.scheduler.emailreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.health.Status;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EmailProcessingHealthIndicatorTest {
+
+	@Test
+	void initialState() {
+		var healthIndicator = new EmailProcessingHealthIndicator();
+
+		assertThat(healthIndicator.health().getStatus()).isEqualTo(Status.UP);
+		assertThat(healthIndicator.isErrors()).isFalse();
+	}
+
+	@Test
+	void setUnhealthy() {
+		var healthIndicator = new EmailProcessingHealthIndicator();
+		healthIndicator.setUnhealthy();
+
+		assertThat(healthIndicator.health().getStatus().getCode()).isEqualTo("RESTRICTED");
+		assertThat(healthIndicator.isErrors()).isTrue();
+	}
+
+	@Test
+	void setHealthy() {
+		var healthIndicator = new EmailProcessingHealthIndicator();
+		healthIndicator.setUnhealthy();
+		healthIndicator.setHealthy();
+
+		assertThat(healthIndicator.health().getStatus()).isEqualTo(Status.UP);
+		assertThat(healthIndicator.isErrors()).isFalse();
+	}
+
+	@Test
+	void resetErrors() {
+		var healthIndicator = new EmailProcessingHealthIndicator();
+		healthIndicator.setUnhealthy();
+		healthIndicator.resetErrors();
+
+		assertThat(healthIndicator.health().getStatus().getCode()).isEqualTo("RESTRICTED");
+		assertThat(healthIndicator.isErrors()).isFalse();
+	}
+}
--- src/test/java/se/sundsvall/supportmanagement/service/scheduler/emailreader/EmailReaderSchedulerTest.java
+++ src/test/java/se/sundsvall/supportmanagement/service/scheduler/emailreader/EmailReaderSchedulerTest.java
@@ -1,30 +1,79 @@
 package se.sundsvall.supportmanagement.service.scheduler.emailreader;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import generated.se.sundsvall.emailreader.Email;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import se.sundsvall.supportmanagement.integration.db.model.EmailWorkerConfigEntity;
+
+import java.util.List;
+import java.util.Set;
 
 @ExtendWith(MockitoExtension.class)
 class EmailReaderSchedulerTest {
 
 	@Mock
 	private EmailReaderWorker emailReaderWorkerMock;
+	@Mock
+	private EmailProcessingHealthIndicator healthIndicatorMock;
+	@Mock
+	private EmailWorkerConfigEntity emailWorkerConfigEntityMock;
+	@Mock
+	private Email emailMock1;
+	@Mock
+	private Email emailMock2;
 
 	@InjectMocks
 	private EmailReaderScheduler emailReaderScheduler;
 
 	@Test
 	void getAndProcessEmails() {
+		// ARRANGE
+		when(emailReaderWorkerMock.getEnabledEmailConfigs()).thenReturn(Set.of(emailWorkerConfigEntityMock));
+		when(emailReaderWorkerMock.getEmailsFromConfig(any())).thenReturn(List.of(emailMock1));
 		// Act
 		emailReaderScheduler.getAndProcessEmails();
 		// Verify
-		verify(emailReaderWorkerMock).getAndProcessEmails();
-		verifyNoMoreInteractions(emailReaderWorkerMock);
+		verify(healthIndicatorMock).resetErrors();
+		verify(emailReaderWorkerMock).getEnabledEmailConfigs();
+		verify(emailReaderWorkerMock).getEmailsFromConfig(same(emailWorkerConfigEntityMock));
+		verify(emailReaderWorkerMock).processEmail(same(emailMock1), same(emailWorkerConfigEntityMock));
+		verify(healthIndicatorMock).isErrors();
+		verify(healthIndicatorMock).setHealthy();
+		verifyNoMoreInteractions(emailReaderWorkerMock, healthIndicatorMock);
+	}
+
+	@Test
+	void getAndProcessEmailsThrowsException() {
+		// ARRANGE
+		when(emailReaderWorkerMock.getEnabledEmailConfigs()).thenReturn(Set.of(emailWorkerConfigEntityMock));
+		when(emailReaderWorkerMock.getEmailsFromConfig(any())).thenReturn(List.of(emailMock1, emailMock2));
+		doThrow(new RuntimeException("error")).when(emailReaderWorkerMock).processEmail(same(emailMock1), any());
+		when(healthIndicatorMock.isErrors()).thenReturn(true);
+		// Act
+		emailReaderScheduler.getAndProcessEmails();
+		// Verify
+		ArgumentCaptor<Email> emailArgumentCaptor = ArgumentCaptor.forClass(Email.class);
+		verify(healthIndicatorMock).resetErrors();
+		verify(emailReaderWorkerMock).getEnabledEmailConfigs();
+		verify(emailReaderWorkerMock).getEmailsFromConfig(same(emailWorkerConfigEntityMock));
+		verify(emailReaderWorkerMock, times(2)).processEmail(emailArgumentCaptor.capture(), same(emailWorkerConfigEntityMock));
+		verify(healthIndicatorMock).setUnhealthy();
+		verify(healthIndicatorMock).isErrors();
+		assertThat(emailArgumentCaptor.getAllValues()).containsExactly(emailMock1, emailMock2);
+		verifyNoMoreInteractions(emailReaderWorkerMock, healthIndicatorMock);
 	}
 
 }
