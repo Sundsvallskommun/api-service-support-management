ac6d9db6dd37565cf0f3e78d9534d86e40566846
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-01-08 14:28:07 +0100

    UF-11506: Fixed mixed up circuitbreaker names

--- src/main/java/se/sundsvall/supportmanagement/integration/employee/EmployeeClient.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/employee/EmployeeClient.java
@@ -1,5 +1,6 @@
 package se.sundsvall.supportmanagement.integration.employee;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static se.sundsvall.supportmanagement.integration.employee.configuration.EmployeeConfiguration.CLIENT_ID;
 
 import generated.se.sundsvall.employee.Employee;
@@ -8,18 +9,16 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import se.sundsvall.supportmanagement.integration.employee.configuration.EmployeeConfiguration;
-import se.sundsvall.supportmanagement.integration.eventlog.configuration.EventlogConfiguration;
 
 @FeignClient(
 	name = CLIENT_ID,
 	url = "${integration.employee.url}",
 	configuration = EmployeeConfiguration.class,
 	dismiss404 = true)
-@CircuitBreaker(name = EventlogConfiguration.CLIENT_ID)
+@CircuitBreaker(name = CLIENT_ID)
 public interface EmployeeClient {
 
 	/**
@@ -28,9 +27,7 @@ public interface EmployeeClient {
 	 * @param  filter containing the filter
 	 * @return        List of employees
 	 */
-	@GetMapping(path = "/employments?filter={filter}", produces = {
-		MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE
-	})
+	@GetMapping(path = "/employments?filter={filter}", produces = APPLICATION_JSON_VALUE)
 	Optional<List<Employee>> getEmployeeInformation(@PathVariable("filter") String filter);
 
 	/**
@@ -40,9 +37,6 @@ public interface EmployeeClient {
 	 * @param  loginName login name of the employee
 	 * @return           PortalPersonData with information about the employee
 	 */
-	@GetMapping(path = "/portalpersondata/{domain}/{loginName}", produces = {
-		MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE
-	})
+	@GetMapping(path = "/portalpersondata/{domain}/{loginName}", produces = APPLICATION_JSON_VALUE)
 	Optional<PortalPersonData> getEmployeeByDomainAndLoginName(@PathVariable("domain") String domain, @PathVariable("loginName") String loginName);
-
 }
