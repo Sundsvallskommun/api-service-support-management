d5530e9bf2b687da577f891131b3805aa178dcbb
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2025-07-07 17:01:48 +0200

    Fix bug in messageExchange sync wrong namespace

--- src/main/java/se/sundsvall/supportmanagement/service/MessageExchangeSyncService.java
+++ src/main/java/se/sundsvall/supportmanagement/service/MessageExchangeSyncService.java
@@ -58,7 +58,7 @@ public class MessageExchangeSyncService {
 
 		final var filter = "sequenceNumber >" + conversationEntity.getLatestSyncedSequenceNumber();
 
-		final var response = messageExchangeClient.getMessages(conversationEntity.getMunicipalityId(), conversationEntity.getNamespace(), conversationEntity.getMessageExchangeId(), filter, Pageable.unpaged());
+		final var response = messageExchangeClient.getMessages(conversationEntity.getMunicipalityId(), messageExchangeNamespace, conversationEntity.getMessageExchangeId(), filter, Pageable.unpaged());
 
 		if (response == null || !response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
 			throw Problem.valueOf(INTERNAL_SERVER_ERROR, "Failed to retrieve messages from Message Exchange");
