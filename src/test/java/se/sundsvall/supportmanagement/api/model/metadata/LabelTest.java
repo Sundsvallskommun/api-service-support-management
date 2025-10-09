0efe5d04a7c6c8896fe6ec10664d4522921fd11f
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-10-09 15:43:43 +0200

    UF-17439: Updated API

--- src/test/java/se/sundsvall/supportmanagement/api/model/metadata/LabelTest.java
+++ src/test/java/se/sundsvall/supportmanagement/api/model/metadata/LabelTest.java
@@ -28,19 +28,28 @@ class LabelTest {
 	void testCreatePattern() {
 		final var classification = "classification";
 		final var displayName = "displayName";
+		final var id = "id";
 		final var labels = List.of(Label.create());
+		final var resourceName = "resourceName";
+		final var resourcePath = "resourcePath";
 		final var name = "name";
 
 		final var bean = Label.create()
 			.withClassification(classification)
 			.withDisplayName(displayName)
+			.withId(id)
 			.withLabels(labels)
-			.withName(name);
+			.withName(name)
+			.withResourceName(resourceName)
+			.withResourcePath(resourcePath);
 
 		assertThat(bean.getClassification()).isEqualTo(classification);
 		assertThat(bean.getDisplayName()).isEqualTo(displayName);
+		assertThat(bean.getId()).isEqualTo(id);
 		assertThat(bean.getLabels()).isEqualTo(labels);
 		assertThat(bean.getName()).isEqualTo(name);
+		assertThat(bean.getResourceName()).isEqualTo(resourceName);
+		assertThat(bean.getResourcePath()).isEqualTo(resourcePath);
 	}
 
 	@Test
