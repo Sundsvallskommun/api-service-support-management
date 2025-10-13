002ec87fd8023721db432b0d3a6924a28d544208
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-10-13 14:43:30 +0200

    UF-17435

--- src/main/java/se/sundsvall/supportmanagement/integration/db/MetadataLabelRepository.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/db/MetadataLabelRepository.java
@@ -4,8 +4,10 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import se.sundsvall.supportmanagement.integration.db.model.MetadataLabelEntity;
 
+@Transactional
 @CircuitBreaker(name = "metadataLabelRepository")
 public interface MetadataLabelRepository extends JpaRepository<MetadataLabelEntity, String> {
 
