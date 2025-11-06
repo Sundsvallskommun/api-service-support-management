13c5243cbd6ebe25fa441495cc95d095bdb46fc1
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-11-06 12:34:58 +0100

    UF-17649: Update script in order to match on namespace as well.

--- tools/db_scripts/populate_errand_labels_metadata_labels_reference.sql
+++ tools/db_scripts/populate_errand_labels_metadata_labels_reference.sql
@@ -1,10 +1,8 @@
 START TRANSACTION;
 
-UPDATE
-    errand_labels AS el
-JOIN metadata_label AS ml ON
-    el.label = ml.name
-SET
-    el.metadata_label_id = ml.id;
+UPDATE errand_labels AS el
+JOIN errand AS e ON e.id = el.errand_id
+JOIN metadata_label AS ml ON ml.name = el.label AND ml.namespace = e.namespace 
+SET el.metadata_label_id = ml.id;
     
 COMMIT;
