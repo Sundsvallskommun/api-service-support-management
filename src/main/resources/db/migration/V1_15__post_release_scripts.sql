d4aa8e838c2fa569f11dc970585a513c7c8a617d
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-11-11 14:28:04 +0100

    UF-17592: Post release cleanup

---
+++ src/main/resources/db/migration/V1_15__post_release_scripts.sql
@@ -0,0 +1,12 @@
+alter table if exists errand_labels 
+   add constraint fk_errand_labels_metadata_label_id_metadata_label_id
+   foreign key (metadata_label_id) 
+   references metadata_label (id);
+   
+drop index idx_errand_labels_errand_id_label on errand_labels;
+
+alter table if exists errand_labels drop column if exists label;
+
+alter table if exists metadata_label drop column if exists name;
+
+drop table if exists label;
