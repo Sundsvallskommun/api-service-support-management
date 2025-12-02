5c234e197a433d39e78e271ea389bea0386bd1f9
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2025-12-02 14:27:05 +0100

    UF-17832: Add access controll to TimeMeasurementService

--- src/main/java/se/sundsvall/supportmanagement/service/TimeMeasurementService.java
+++ src/main/java/se/sundsvall/supportmanagement/service/TimeMeasurementService.java
@@ -1,29 +1,25 @@
 package se.sundsvall.supportmanagement.service;
 
-import static org.zalando.problem.Status.NOT_FOUND;
+import static generated.se.sundsvall.accessmapper.Access.AccessLevelEnum.R;
+import static generated.se.sundsvall.accessmapper.Access.AccessLevelEnum.RW;
 import static se.sundsvall.supportmanagement.service.mapper.TimeMeasurementMapper.toTimeMeasurements;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
-import org.zalando.problem.Problem;
 import se.sundsvall.supportmanagement.api.model.errand.TimeMeasurement;
-import se.sundsvall.supportmanagement.integration.db.ErrandsRepository;
 
 @Service
 public class TimeMeasurementService {
 
-	private static final String ENTITY_NOT_FOUND = "An errand with id '%s' could not be found in namespace '%s' for municipality with id '%s'";
+	private final AccessControlService accessControlService;
 
-	private final ErrandsRepository repository;
-
-	public TimeMeasurementService(final ErrandsRepository repository) {
-		this.repository = repository;
+	public TimeMeasurementService(final AccessControlService accessControlService) {
+		this.accessControlService = accessControlService;
 	}
 
 	public List<TimeMeasurement> getErrandTimeMeasurements(final String namespace, final String municipalityId, final String errandId) {
-		return repository.findByIdAndNamespaceAndMunicipalityId(errandId, namespace, municipalityId)
-			.map(errandEntity -> toTimeMeasurements(errandEntity.getTimeMeasures()))
-			.orElseThrow(() -> Problem.valueOf(NOT_FOUND, String.format(ENTITY_NOT_FOUND, errandId, namespace, municipalityId)));
+		var errandEntity = accessControlService.getErrand(namespace, municipalityId, errandId, false, R, RW);
+		return toTimeMeasurements(errandEntity.getTimeMeasures());
 	}
 
 }
