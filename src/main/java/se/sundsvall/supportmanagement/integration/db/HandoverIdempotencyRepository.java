063123914bdbdd7c1c9654ff434a7b48dca2fa97
Zara Lundgren Pettersson <zaralundgrenpettersson@gmail.com>
2026-06-11 08:45:20 +0200

    add CirciutBreaker to repository

--- src/main/java/se/sundsvall/supportmanagement/integration/db/HandoverIdempotencyRepository.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/db/HandoverIdempotencyRepository.java
@@ -1,9 +1,11 @@
 package se.sundsvall.supportmanagement.integration.db;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.sundsvall.supportmanagement.integration.db.model.HandoverIdempotencyEntity;
 
+@CircuitBreaker(name = "handoverIdempotencyRepository")
 public interface HandoverIdempotencyRepository extends JpaRepository<HandoverIdempotencyEntity, String> {
 
 	Optional<HandoverIdempotencyEntity> findBySourceErrandIdAndTargetNamespaceAndTargetMunicipalityId(String sourceErrandId, String targetNamespace, String targetMunicipalityId);
