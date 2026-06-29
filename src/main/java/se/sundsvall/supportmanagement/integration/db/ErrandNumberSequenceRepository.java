66c99dba0d2082c4bc720c3098bc3c03a70ea5f4
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2026-06-29 14:21:53 +0200

    DRAKEN-4525: Fix dead lock on ErrandNumberGenerator

--- src/main/java/se/sundsvall/supportmanagement/integration/db/ErrandNumberSequenceRepository.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/db/ErrandNumberSequenceRepository.java
@@ -1,13 +1,16 @@
 package se.sundsvall.supportmanagement.integration.db;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import se.sundsvall.supportmanagement.integration.db.model.ErrandNumberSequenceEntity;
 
 @CircuitBreaker(name = "errandNumberSequenceRepository")
 public interface ErrandNumberSequenceRepository extends JpaRepository<ErrandNumberSequenceEntity, String> {
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<ErrandNumberSequenceEntity> findByNamespaceAndMunicipalityId(String namespace, String municipalityId);
 
 }
