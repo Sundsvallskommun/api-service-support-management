ddf518d487a647ec91dd7c622bdabb9bca8f0d2a
Zara Lundgren Pettersson <zaralundgrenpettersson@gmail.com>
2026-08-11 14:27:22 +0200

    delete unused file

--- src/main/java/se/sundsvall/supportmanagement/integration/db/JsonParameterRepository.java
+++
@@ -1,9 +0,0 @@
-package se.sundsvall.supportmanagement.integration.db;
-
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.sundsvall.supportmanagement.integration.db.model.JsonParameterEntity;
-
-@CircuitBreaker(name = "jsonParameterRepository")
-public interface JsonParameterRepository extends JpaRepository<JsonParameterEntity, String> {
-}
