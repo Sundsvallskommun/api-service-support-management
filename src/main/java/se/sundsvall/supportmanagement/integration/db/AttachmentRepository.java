4426033927cb646cf570045f04a9aacf24ed1c1b
Michael Wallner <Michael.Wallner@sundsvall.se>
2026-08-24 10:28:31 +0200

    DRAKEN-4607: Changes after rebase

--- src/main/java/se/sundsvall/supportmanagement/integration/db/AttachmentRepository.java
+++ src/main/java/se/sundsvall/supportmanagement/integration/db/AttachmentRepository.java
@@ -4,8 +4,6 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import se.sundsvall.supportmanagement.integration.db.model.AttachmentEntity;
@@ -14,8 +12,6 @@ import se.sundsvall.supportmanagement.integration.db.model.AttachmentEntity;
 public interface AttachmentRepository extends JpaRepository<AttachmentEntity, String> {
 
 	Optional<AttachmentEntity> findByNamespaceAndMunicipalityIdAndErrandEntityIdAndId(final String namespace, final String municipalityId, final String errandId, final String id);
-
-	List<AttachmentEntity> findByNamespaceAndMunicipalityIdAndIdIn(final String namespace, final String municipalityId, final List<String> ids);
 
 	@Query("SELECT a.id FROM AttachmentEntity a WHERE a.hash IS NULL")
 	List<String> findIdsByHashIsNull(Pageable pageable);
