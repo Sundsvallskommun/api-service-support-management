c8e34a7369cfe427ba61e65aa276392405e30249
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2026-08-31 16:06:57 +0200

    DRAKEN-4801:

--- src/test/java/se/sundsvall/supportmanagement/integration/db/model/StakeholderEntityTest.java
+++ src/test/java/se/sundsvall/supportmanagement/integration/db/model/StakeholderEntityTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanEquals;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanHashCode;
-import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToStringExcluding;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,7 +21,7 @@ class StakeholderEntityTest {
 			hasValidGettersAndSetters(),
 			hasValidBeanHashCode(),
 			hasValidBeanEquals(),
-			hasValidBeanToString()));
+			hasValidBeanToStringExcluding("errandEntity")));
 	}
 
 	@Test
