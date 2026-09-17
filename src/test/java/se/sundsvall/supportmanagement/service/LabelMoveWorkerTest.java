8eefc2431d5ba0127a9248fe85a4b0e5b5fb0b53
Zara Lundgren Pettersson <zaralundgrenpettersson@gmail.com>
2026-09-17 10:18:17 +0200

    fix review comment

--- src/test/java/se/sundsvall/supportmanagement/service/LabelMoveWorkerTest.java
+++ src/test/java/se/sundsvall/supportmanagement/service/LabelMoveWorkerTest.java
@@ -162,6 +162,32 @@ class LabelMoveWorkerTest {
 			.containsExactly(movedId);
 	}
 
+	@Test
+	void rebuildLabels_emptyAccessLabels_leftUntouched() {
+		var errand = ErrandEntity.create()
+			.withAccessLabels(List.of())
+			.withLabels(List.of(ErrandLabelEmbeddable.create().withMetadataLabelId("stale-id")));
+
+		worker.rebuildLabels(errand);
+
+		assertThat(errand.getLabels())
+			.extracting(ErrandLabelEmbeddable::getMetadataLabelId)
+			.containsExactly("stale-id");
+	}
+
+	@Test
+	void rebuildLabels_nullAccessLabels_leftUntouched() {
+		var errand = ErrandEntity.create()
+			.withAccessLabels(null)
+			.withLabels(List.of(ErrandLabelEmbeddable.create().withMetadataLabelId("stale-id")));
+
+		worker.rebuildLabels(errand);
+
+		assertThat(errand.getLabels())
+			.extracting(ErrandLabelEmbeddable::getMetadataLabelId)
+			.containsExactly("stale-id");
+	}
+
 	@AfterEach
 	void verifyNoMoreInteractionsOnMocks() {
 		verifyNoMoreInteractions(errandsRepositoryMock, metadataLabelRepositoryMock, errandServiceMock);
